30fb6f9ca077c66442f5e5fc3d86068880d616d4
puneetvashisht <puneetvashisht@gmail.com>
2016-06-14 12:23:42 +0530

    7

---
+++ SpringDay2/src/main/java/com/cog/TransferService.java
@@ -0,0 +1,25 @@
+package com.cog;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransferService {
+	
+	public double checkBalance(long account){
+//		System.out.println("Account number is " +  account);
+		
+		double balance = account * Math.random();		
+		return balance;
+	}
+
+	public boolean fundsTransfer(long account1, long account2, double amount){
+//		System.out.println("Account number is " +  account1 + " : " + account2);
+		
+		if(account1 != account2){
+			return true;
+		}
+		
+		return false;
+	}
+		
+}
