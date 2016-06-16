8638efd3fddd0228d1152a8767ddf249d771e430
puneetvashisht <puneetvashisht@gmail.com>
2016-06-16 11:36:35 +0530

    15

--- JPAWithoutMaven/src/com/cog/repo/MemberRepository.java
+++ JPAWithoutMaven/src/com/cog/repo/MemberRepository.java
@@ -5,59 +5,71 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import com.cog.entity.Address;
+import com.cog.entity.Employee;
 import com.cog.entity.Member;
 
-
 public class MemberRepository {
-	
-	
-	EntityManagerFactory emf = Persistence.
-			 createEntityManagerFactory("mypersistanceunit");
-    
-//	SessionFactory sesssionFactory = new Configuration().configure().buildSessionFactory();
+
+	EntityManagerFactory emf = Persistence
+			.createEntityManagerFactory("mypersistanceunit");
+
+	// SessionFactory sesssionFactory = new
+	// Configuration().configure().buildSessionFactory();
 	public static void main(String[] args) {
-		Member member = new Member("priya2@gmail.com", "dskg");
-		MemberRepository repository =  new MemberRepository();
+		MemberRepository repository = new MemberRepository();
+		/*Member member = new Member("priya2@gmail.com", "dskg");
+		repository.addMember(member);*/
 		
-		repository.addMember(member);
+		Address address = new Address(12, "GK", "Delhi");
+		Employee emp = new Employee("Ravi", address);
 		
-//		repository.fetchMember(2);
+		repository.addEmployee(emp);
+
+		// repository.fetchMember(2);
 	}
-	
-	public void fetchMember(int id){
-//		Session session = sesssionFactory.openSession();
-		
+
+	public void addEmployee(Employee emp) {
 		EntityManager em = emf.createEntityManager();
-	     
-		Member member =  em.find(Member.class, id);
-		System.out.println(member);
-	}
-	
-	public boolean addMember(Member member) {
-		
-		EntityManager em = emf.createEntityManager();
-//		Session session = sesssionFactory.openSession();
+		// Session session = sesssionFactory.openSession();
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
-		
-		em.persist(member);
-		
-		
+
+		em.persist(emp);
+
 		transaction.commit();
 		em.close();
-	// JDBC Template Code
-		/*String SQL = "insert into member (email, password) values (?,?)";
-		int count = jdbcTemplate.update(SQL, member.getEmail(), member.getPassword());
-		if(count>=1){
-			return true;
-		}
-		return false;*/
-		//Hibernate code
-		
-		
-		
+	}
+
+	public void fetchMember(int id) {
+		// Session session = sesssionFactory.openSession();
+
+		EntityManager em = emf.createEntityManager();
+
+		Member member = em.find(Member.class, id);
+		System.out.println(member);
+	}
+
+	public boolean addMember(Member member) {
+
+		EntityManager em = emf.createEntityManager();
+		// Session session = sesssionFactory.openSession();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+
+		em.persist(member);
+
+		transaction.commit();
+		em.close();
+		// JDBC Template Code
+		/*
+		 * String SQL = "insert into member (email, password) values (?,?)"; int
+		 * count = jdbcTemplate.update(SQL, member.getEmail(),
+		 * member.getPassword()); if(count>=1){ return true; } return false;
+		 */
+		// Hibernate code
+
 		return false;
 	}
-	
 
 }
