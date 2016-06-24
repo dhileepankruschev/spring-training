a377542d8f30400b96a6dd3bbcab5e1d5f55a568
puneetvashisht <puneetvashisht@gmail.com>
2016-06-24 11:26:28 +0530

    31

--- SpringRest/src/main/java/com/cog/user/controller/UserRestController.java
+++ SpringRest/src/main/java/com/cog/user/controller/UserRestController.java
@@ -1,5 +1,8 @@
 package com.cog.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cog.user.entity.User;
+import com.cog.user.service.UserService;
 
 
 @RestController
 public class UserRestController {
 	
-	@RequestMapping(path="/user/", method=RequestMethod.GET)
-	public ResponseEntity<User> getUser(){
-		User user = new User("ravi@gmail.com", "ravi");
-		System.out.println(user);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+	
+	@Autowired
+	UserService userService;  //Service which will do all data retrieval/manipulation work
+
+	@RequestMapping(value = "/user/", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> listAllUsers() {
+		List<User> users = userService.findAllUsers();
+		if(users.isEmpty()){
+			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+		}
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
 	}
 
 }
--- SpringRest/src/main/java/com/cog/user/entity/User.java
+++ SpringRest/src/main/java/com/cog/user/entity/User.java
@@ -1,32 +1,84 @@
 package com.cog.user.entity;
 
 public class User {
-	String email;
-	String password;
-	@Override
-	public String toString() {
-		return "User [email=" + email + ", password=" + password + "]";
-	}
+private long id;
+	
+	private String name;
+	
+	private int age;
+	
+	private double salary;
+
 	public User(){
-		
-	}
-	public User(String email, String password) {
-		super();
-		this.email = email;
-		this.password = password;
+		id=0;
 	}
 	
-	public String getEmail() {
-		return email;
+	public User(long id, String name, int age, double salary){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+	
+	public long getId() {
+		return id;
 	}
-	public String getPassword() {
-		return password;
+
+	public void setId(long id) {
+		this.id = id;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age
+				+ ", salary=" + salary + "]";
+	}
+
 
 }
